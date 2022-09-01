06688b5a1018d899103c1d8bd9382ecade604518
Ajitkumar6 <ajitcareerit@gmail.com>
2022-09-01 12:59:21 +0530

    Photon Interview Task

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -3,6 +3,7 @@ import { AiOutlineStar, AiFillStar } from "react-icons/ai";
 
 //<AiOutlineStar/>
 //<AiFillStar/>
+// Photon Interview Task
 
 export default function StarRating({ starCount }) {
   return (
